e2041fc80eb91a4c64d74e723a1a2d4b997cfe6c
Hang Cui <oscarcuihang@hotmail.com>
2015-03-15 13:08:12 -0500

    update aboutus page

--- pages/about/aboutus.php
+++ pages/about/aboutus.php
@@ -1,9 +1,28 @@
 <?php include '../templates/header.html'; ?>
 
-
+<!--
 <style type="text/css">
 
-</style><!-- Custom Fonts CSS -->
+.people
+{
+  position: relative;
+  text-align: center;
+  padding-top: 200px;
+  color: black;
+  margin: 0;
+}
+.img
+{
+
+}
+.info
+{
+
+}
+
+
+</style>-->
+    <link href="whatever.css" rel="stylesheet">
 
 
 <?php include '../templates/navbar.html'; ?>
@@ -12,7 +31,27 @@
     
 
     <div class="container">
+      
+      <!--<div class="people">
+        <div class="img">
+          <img src="hc.png" width="320" height="320" alt="hc">
+        </div>
+        <div class="info">
+          asdasdasd
+        </div>
+      </div>
 
+      <div class="people">
+        <img src="hc.png" width="320" height="320" alt="hc">
+      </div>
+      <div class="people">
+        <img src="hc.png" width="320" height="320" alt="hc">
+      </div>
+      <div class="people">
+        <img src="hc.png" width="320" height="320" alt="hc">
+      </div>-->
+
+      
     </div>
 
 
