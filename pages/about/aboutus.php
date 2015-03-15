707f9ce23fbcbc4337b80aad48f10cd78f40f400
Hang Cui <oscarcuihang@hotmail.com>
2015-03-14 20:05:28 -0500

    create a page for about us

---
+++ pages/about/aboutus.php
@@ -0,0 +1,19 @@
+<?php include '../templates/header.html'; ?>
+
+
+<style type="text/css">
+
+</style><!-- Custom Fonts CSS -->
+
+
+<?php include '../templates/navbar.html'; ?>
+
+
+    
+
+    <div class="container">
+
+    </div>
+
+
+<?php include '../templates/footer.html'; ?>
