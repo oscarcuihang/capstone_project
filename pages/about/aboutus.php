211ff044c6bd795e8a4c44dca96fd0dfd9c8531d
Hang Cui <oscarcuihang@hotmail.com>
2015-04-06 15:56:00 -0500

    update about us page

--- pages/about/aboutus.php
+++ pages/about/aboutus.php
@@ -7,12 +7,89 @@
 
 
 <?php include '../templates/navbar.html'; ?>
-
-
-    
-
     <div class="container">
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">About Us
+                    <small>Road Trip Planner</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
 
+        <!-- Project One -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="hc.png" alt="" style="width:300px;height:300px">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Hang CUI</h3>
+                <h4>Lao Da</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+                <a class="btn btn-primary" href="#">View <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Two -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="zh.png" alt="" style="width:300px;height:300px">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Zhenni HU</h3>
+                <h4>Subheading</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
+                <a class="btn btn-primary" href="#">View <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Three -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="123.gif" alt="" style="width:300px;height:300px">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Zhongrong JIAN</h3>
+                <h4>Subheading</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
+                <a class="btn btn-primary" href="#">View <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Project Four -->
+        <div class="row">
+
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="bx.png" alt="" style="width:300px;height:300px">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Bowei XU</h3>
+                <h4>Dobi</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem officiis illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
+                <a class="btn btn-primary" href="#">View <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
     </div>
 
 
