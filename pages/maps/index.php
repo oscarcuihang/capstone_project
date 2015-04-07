1c0e62593ab87099ff82b2150221efe6301b055a
Hang Cui <oscarcuihang@hotmail.com>
2015-04-06 19:35:04 -0500

    add searchable option for map

--- pages/maps/index.php
+++ pages/maps/index.php
@@ -1,5 +1,38 @@
 <?php include '../templates/header.html'; ?>
 
+ <style>
+     .controls {
+        margin-top: 16px;
+        border: 1px solid transparent;
+        border-radius: 2px 0 0 2px;
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        height: 32px;
+        outline: none;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+      }
+
+      #pac-input {
+        background-color: #fff;
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+        margin-left: 12px;
+        padding: 0 11px 0 13px;
+        text-overflow: ellipsis;
+        width: 400px;
+      }
+
+      #pac-input:focus {
+        border-color: #4d90fe;
+      }
+
+      .pac-container {
+        font-family: Roboto;
+      }
+
+    </style>
+
 <style type="text/css">
 	html,body{
 		height:100%;
@@ -76,8 +109,9 @@
 
     </div>
     <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-                    
-	<div id="map-canvas"></div>
+                      <input id="pac-input" class="controls" type="text"
+        placeholder="Enter a location">
+    <div id="map-canvas"></div>
     <div id="push"></div>
 
     <div class="container">
@@ -91,53 +125,87 @@
 <script type='text/javascript' src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 -->
 
-<script type='text/javascript' src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
+<script type='text/javascript' src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed&signed_in=true&libraries=places"></script>
 
 <script type='text/javascript'>
-  $(document).ready(function() {
-    function initialize() {
-      getLocation(); 
+ 
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(36.7277417,-96.6817111),
+    zoom: 5
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+  var types = document.getElementById('type-selector');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
     }
-    google.maps.event.addDomListener(window, 'load', initialize);
 
-    function getLocation(){
-      if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error);
-      } else {
-        // default location
-      }
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
     }
 
-    function success(position){
-      var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
 
-      var mapOptions = {
-        center: latlng,
-        zoom: 12
-      };
-      var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-
-      function error(msg){
-        if (msg.code == 1) {
-            //PERMISSION_DENIED 
-        } else if (msg.code == 2) {
-            //POSITION_UNAVAILABLE 
-        } else {
-        }   //TIMEOUT
-      }
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    google.maps.event.addDomListener(radioButton, 'click', function() {
+      autocomplete.setTypes(types);
     });
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
   </script>
         
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-40413119-1', 'bootply.com');
-    ga('send', 'pageview');
-  </script>
+  
 
   <script>
 	$(document).ready(function(){
