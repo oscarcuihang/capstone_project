8b82d96a7bde312e41f6d805d96ced4002146d90
Hang Cui <oscarcuihang@hotmail.com>
2015-04-13 16:25:13 -0500

    update home page search option and default option
    update home page search option and default option

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,13 @@
 <?php include '../templates/header.html'; ?>
 
+<style type="text/css">
+    #imagestyle {
+        width:440px;
+        height:250px
+
+    }
+</style><!-- Custom Fonts CSS -->
+
 <?php include '../templates/navbar.html'; ?>
 
 
@@ -11,7 +19,7 @@
             <form method="POST" action="">
             <div id="custom-search-input">
                 <div class="input-group col-md-12">
-                    <input type="text" class="form-control input-lg" placeholder="Search Anything" />
+                    <input type="text" name="search" class="form-control input-lg" placeholder="Search Anything" />
                     <span class="input-group-btn">
                         <button class="btn btn-info btn-lg" type="submit">
                             <i class="glyphicon glyphicon-search"></i>
@@ -23,9 +31,55 @@
 
         </div>
   </div>
-
   <hr>
 
+<?php if(isset($_POST['search']))
+{
+?>
+  <!-- Project One -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="">
+                    <img class="img-responsive" src="" alt="" id = "imagestyle">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Searched Title</h3>
+                <h4>Searched Cata</h4>
+                <p>Searched PostDate</p>
+                <p>Searched Author</p>
+                <p>Searched Details</p>
+                <a class="btn btn-primary" href="">View <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+        <hr>
+<?php
+}
+
+else{
+?>
+      <!-- Project One -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="">
+                    <img class="img-responsive" src="" alt="" id = "imagestyle">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Default Title</h3>
+                <h4>Default Cata</h4>
+                <p>Default PostDate</p>
+                <p>Default Author</p>
+                <p>Default Details</p>
+                <a class="btn btn-primary" href="">View <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- /.row -->
+        <hr>
+<?php
+}
+?>
 </div>
 
 
