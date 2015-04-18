1d2dfe5f6e7740f0fefd9bf3006404d76562bdd0
Hang Cui <oscarcuihang@hotmail.com>
2015-04-17 19:24:18 -0500

    add a single journal page

---
+++ pages/journal/viewjournal.php
@@ -0,0 +1,22 @@
+<?php include '../templates/header.html'; ?>
+<?php include '../templates/navbar.html'; ?>
+
+
+    
+
+    <div class="container">
+
+     
+<?php
+
+	$pk=$_POST['pk'];
+	echo $pk;
+?>
+
+    </div> <!-- /container -->
+
+
+<?php include '../templates/footer.html'; ?>
+
+
+
