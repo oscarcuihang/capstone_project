8744b73fee8b0f5ea4ceda31b92486c7dc9409f8
Hang Cui <oscarcuihang@hotmail.com>
2015-04-26 03:29:24 -0500

    add delete journal function page

---
+++ pages/journal/deletejournal.php
@@ -0,0 +1,16 @@
+<?php include '../templates/header.html'; ?>
+<?php include '../templates/navbar.html'; ?>
+
+
+    
+
+    <div class="container">
+      <a class="btn btn-warning" href="/capstone_project/pages/journal/viewjournal.php?id=<?php echo $journal_id; ?>">Confirm <span class="glyphicon glyphicon-chevron-right"></span></a>
+     
+      <a class="btn btn-danger" href="/capstone_project/pages/journal/viewjournal.php?id=<?php echo $journal_id; ?>">Cancel <span class="glyphicon glyphicon-chevron-right"></span></a>
+            
+
+    </div> <!-- /container -->
+
+
+<?php include '../templates/footer.html'; ?>
