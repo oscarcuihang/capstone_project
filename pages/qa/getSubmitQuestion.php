6493e4a188471a996a16c3957dcf9925d5bed1d6
Hunter6 <bx3g3@mail.missouri.edu>
2015-04-26 21:04:59 -0500

    ask question function finished

---
+++ pages/qa/getSubmitQuestion.php
@@ -0,0 +1,21 @@
+
+<?php include '../templates/header.html'; ?>
+<?php //include '../templates/navbar.html'; ?>
+
+<?php
+
+
+if(!isset($_SESSION['id'])){
+	die ("Miaomiao shi huai ren!");
+}
+ if(isset($_POST["editor"])){
+		$id=$_SESSION["id"];
+		$editor=$_POST["editor"];
+		$query = "INSERT INTO question VALUES(DEFAULT,'$id','$editor',DEFAULT,0,0)";
+		mysql_query($query,$conn) or die(mysql_error());
+				
+		mysql_query("INSERT INTO userLog VALUES(DEFAULT, $id, '$ip', DEFAULT, 'create/post question')");
+
+		header('Location: index.php');
+		
+?>
