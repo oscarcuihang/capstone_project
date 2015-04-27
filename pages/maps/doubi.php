754962c6f519ec86353dc35a1f95fe958377c401
Zhongrong <jiansmith.zj@gmail.com>
2015-04-26 21:47:40 -0500

    just for fun~

--- pages/maps/doubi.php
+++ pages/maps/doubi.php
@@ -4,19 +4,14 @@
 
     
 
-    <div class="container">
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
-      </div>
-
-    </div> <!-- /container -->
-	<button class = 'sign-in-btn-nav'>fsfsdfd</button>
+    <div style = "position:absolute;left:50%;width:500px;">
+		<div class = "panel panel-success" style = "position:relative;right:50%;">
+			<div class = "panel-heading">miao</div>
+			<div class = "panel-body">
+				<textarea rows = "10" cols = "50">
+				</textarea>
+			</div>
+		</div>
+	</div>
 
 <?php include '../templates/footer.html'; ?>
